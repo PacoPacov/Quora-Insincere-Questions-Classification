9a0b3fc405c5096737986c483832e01b0a8823f1
Panayotov, Panayotov <Panayot.Panayotov@experian.com>
2019-03-05 09:22:23 +0200

    update packaging of the repo

--- insincere_questions_classification/helper_functions/__init__.py
+++ insincere_questions_classification/helper_functions/__init__.py
@@ -1,15 +1,15 @@
-from helper_functions.functions import (
+from .functions import (
     downsampling, upsampling, train_model, export_model, load_model, plot_confusion_matrix, log_model)
-from helper_functions.custom_transformers import FactorExctractor
+from .custom_transformers import FactorExctractor
 
 
-__all__ = [
-    'downsampling',
-    'upsampling',
-    'train_model',
-    'export_model',
-    'load_model',
-    'plot_confusion_matrix',
-    'load_model',
-    'FactorExctractor'
-]
+# __all__ = [
+#     'downsampling',
+#     'upsampling',
+#     'train_model',
+#     'export_model',
+#     'load_model',
+#     'plot_confusion_matrix',
+#     'load_model',
+#     'FactorExctractor'
+# ]
--- insincere_questions_classification/main/__init__.py
+++ insincere_questions_classification/main/__init__.py
@@ -1,10 +1,10 @@
-from insincere_questions_classification.main.predict_sents import (
+from .predict_sents import (
     find_types_of_sents_in_text, clean_raw_data, data_prep, make_prediction)
 
 
-__all__ = [
-    'find_types_of_sents_in_text',
-    'clean_raw_data',
-    'data_prep',
-    'make_prediction'
-]
+# __all__ = [
+#     'find_types_of_sents_in_text',
+#     'clean_raw_data',
+#     'data_prep',
+#     'make_prediction'
+# ]
