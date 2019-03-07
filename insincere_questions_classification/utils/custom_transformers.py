389dfd8736bbafe55e82196cd251315b09f8aa92
Panayotov, Panayotov <Panayot.Panayotov@experian.com>
2019-03-07 21:51:27 +0200

    Update the structure of  custom transformer.

--- insincere_questions_classification/utils/custom_transformers.py
+++ insincere_questions_classification/utils/custom_transformers.py
@@ -1,13 +1,19 @@
 from sklearn.base import TransformerMixin, BaseEstimator
 
 
-class FactorExctractor(TransformerMixin, BaseEstimator):
-    """Custom Transformer that extracts specified column."""
-    def __init__(self, factor):
-        self.factor = factor
+class FeatureExctractor(BaseEstimator, TransformerMixin):
+    """Custom Transformer that extracts specified column(s).
+    :param feature: str or list od string that specifies which features will be extracted.
+
+    Note: FeatureExctractor is not designed to handle data grouped by sample. 
+    (e.g. a list of dicts).  If your data is structured this way, consider a
+    transformer along the lines of `sklearn.feature_extraction.DictVectorizer`.
+    """
+    def __init__(self, feature):
+        self.feature = feature
 
     def transform(self, dataset):
-        return dataset[self.factor]
+        return dataset[self.feature]
 
     def fit(self, *_):
         return self
